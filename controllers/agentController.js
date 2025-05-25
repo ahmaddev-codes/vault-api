448c574ffb64e1cda9bbd1f45a2f086ca000e6ce
Tijani Ahmad <omegabliss23@gmail.com>
2025-05-25 22:29:30 +0100

    Update: controlling to get all agents

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -1,12 +1,12 @@
-const jwt = require('jsonwebtoken');
+const { agent } = require('supertest');
 const Agent = require('../models/Agent');
 const generateToken = require('../utils/generateToken');
 
-exports.registerAgent = async (req, res) => {
+const registerAgent = async (req, res) => {
   const { name, email, password } = req.body;
   const agentExists = await Agent.findOne({ email });
 
-  if (agentExists) return res.status(400).json({ message: 'Agent already exists'});
+  if (agentExists) return res.status(400).json({ message: 'Agent already exists' });
 
   const agent = await Agent.create({ name, email, password });
 
@@ -15,14 +15,28 @@ exports.registerAgent = async (req, res) => {
       _id: agent._id,
       name: agent.name,
       email: agent.email,
-      token: generateToken(agent._id),
+      token: generateToken(agent._id)
     });
   } else {
     res.status(400).json({ message: 'Invalid agent data' });
   }
 };
 
-exports.authAgent = async (req, res) => {
+// Get all Agent
+const getAllAgents = async (req, res) => {
+  try {
+    const agents = await Agent.find({}, 'name email')
+    res.status(200).json({
+      TotalAgents: agents.length,
+      Agents: agents
+    })
+  } catch (error) {
+    res.status(500).json({ error: "Server error", message: error.message })
+  }
+}
+
+// Authenticate and login Agent
+const authAgent = async (req, res) => {
   const { email, password } = req.body;
   const agent = await Agent.findOne({ email });
 
@@ -37,3 +51,5 @@ exports.authAgent = async (req, res) => {
     res.status(401).json({ message: 'Invalid credentials' });
   }
 };
+
+module.exports = { registerAgent, authAgent, getAllAgents }
--- controllers/intelController.js
+++ controllers/intelController.js
@@ -1,19 +1,22 @@
 const Intel = require('../models/Intel');
 
-exports.createIntel = async (req, res) => {
-  const { title, description, location } = req.body;
-  const intel = new Intel({ title, description, location, agentId: req.agent._id });
+const createIntel = async (req, res) => {
+  const { agentId, title, description, location } = req.body;
+  const intel = new Intel({ title, description, location, agentId });
   const created = await intel.save();
   res.status(201).json(created);
 };
 
-exports.getMyIntel = async (req, res) => {
-  const intel = await Intel.find({ agentId: req.agent._id });
+const getMyIntel = async (req, res) => {
+  const { agentId } = req.body
+  const intel = await Intel.find({ agentId });
   res.json(intel);
 };
 
-exports.updateIntel = async (req, res) => {
-  const intel = await Intel.findById(req.params.id);
+const updateIntel = async (req, res) => {
+  const {agentId} = req.body
+  const intel = await Intel.findById(agentId);
+
   if (intel && intel.agentId.toString() === req.agent._id.toString()) {
     intel.title = req.body.title || intel.title;
     intel.description = req.body.description || intel.description;
@@ -25,7 +28,7 @@ exports.updateIntel = async (req, res) => {
   }
 };
 
-exports.deleteIntel = async (req, res) => {
+const deleteIntel = async (req, res) => {
   const intel = await Intel.findById(req.params.id);
   if (intel && intel.agentId.toString() === req.agent._id.toString()) {
     await intel.remove();
@@ -34,3 +37,10 @@ exports.deleteIntel = async (req, res) => {
     res.status(404).json({ message: 'Intel not found or unauthorized' });
   }
 };
+
+module.exports = {
+  createIntel,
+  getMyIntel,
+  updateIntel,
+  deleteIntel
+}
